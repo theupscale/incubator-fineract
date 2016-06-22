38a984a88d03051594e7f008d47359c26b10b6ab
maektwain <saranshisat@icloud.com>
2016-06-22 20:29:05 +0530

    USF-9

--- fineract-provider/src/main/resources/sql/migrations/core_db/V99_1__client_fatherName.sql
+++ fineract-provider/src/main/resources/sql/migrations/core_db/V99_1__client_fatherName.sql
@@ -1,2 +1,1 @@
-ALTER TABLE  `m_client` ADD  `fathername` VARCHAR(100) DEFAULT NULL ,
-ADD  `email_Address` VARCHAR(100) DEFAULT NULL ;
+ALTER TABLE  `m_client` ADD  `fathername` VARCHAR(100) DEFAULT NULL;
